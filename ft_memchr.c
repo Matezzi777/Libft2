ef118f036fb06a525224c01806fa5c13fb3a3d57
Matezzi777 <matezzi777@gmail.com>
2025-02-11 03:11:23 +0100

    strncmp, memchr done

--- ft_memchr.c
+++ ft_memchr.c
@@ -2,5 +2,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	size_t			i;
+	unsigned char	*s2;
 
+	s2 = (unsigned char *)s;
+	while (i < n)
+	{
+		if (*s2 == c)
+			return (s2);
+		s2++;
+	}
+	return (NULL);
 }
