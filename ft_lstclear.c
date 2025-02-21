4d1ebe41081768877310dde129c1ba3eba4d2f59
Matezzi777 <matezzi777@gmail.com>
2025-02-21 21:28:29 +0100

    blabla

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -2,5 +2,15 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*node;
+	t_list	*to_del;
 
+	node = *lst;
+	while (node->next)
+	{
+		del(node->content);
+		to_del = node;
+		node = node->next;
+		free(to_del);
+	}
 }
